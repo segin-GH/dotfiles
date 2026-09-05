17d45232502bda6f05342d7506665f2e65ff48b6
segin-GH <segin.bytes@gmail.com>
2026-09-06 00:16:56 +0530

    i3 display layout fix, nvim treesitter main branch + neorg cleanup, wallpapers

--- nvim/init.lua
+++ nvim/init.lua
@@ -145,16 +145,11 @@ vim.api.nvim_set_keymap("n", "zh", "zH", { noremap = true })
 vim.keymap.set("n", "<C-g>", ":LspClangdSwitchSourceHeader<CR>", { desc = "Switch between source/header file" })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
---
--- This is a fix for Unable to load nvim-treesitter.ts_utils #1715
 
+-- Enable treesitter-based highlighting (nvim-treesitter v1.x no longer does this itself)
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "norg", "neorg" },
 	callback = function()
-		if pcall(vim.treesitter.start) then
-			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-		end
+		pcall(vim.treesitter.start)
 	end,
 })
 
@@ -178,7 +173,27 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 	pattern = "*.overlay",
